4a8dfb42338d7af132a8c0734e0c64d40ce607ce
Ciprian Salomir <ciprian.salomir@gmail.com>
2025-07-22 16:44:23 +0300

    config update

--- app/src/main/java/com/example/mvi_clean_demo/ui/theme/Theme.kt
+++ app/src/main/java/com/example/mvi_clean_demo/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.mvi_clean_demo.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
